5e15ee996938b197ac97ebaa1b5ae8c5bcf128f3
Akash Dubey <101584901+Akash6222@users.noreply.github.com>
2023-06-29 22:50:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,22 +25,6 @@ theme_color='default'
 
 SASSC_OPT="-M -t expanded"
 
-if [[ "$(command -v gnome-shell)" ]]; then
-  gnome-shell --version
-  SHELL_VERSION="$(gnome-shell --version | cut -d ' ' -f 3 | cut -d . -f -1)"
-  if [[ "${SHELL_VERSION:-}" -ge "44" ]]; then
-    GS_VERSION="44-0"
-  elif [[ "${SHELL_VERSION:-}" -ge "42" ]]; then
-    GS_VERSION="42-0"
-  elif [[ "${SHELL_VERSION:-}" -ge "40" ]]; then
-    GS_VERSION="40-0"
-  else
-    GS_VERSION="3-32"
-  fi
-  else
-    echo "'gnome-shell' not found, using styles for last gnome-shell version available."
-    GS_VERSION="44-0"
-fi
 
 usage() {
 cat << EOF
